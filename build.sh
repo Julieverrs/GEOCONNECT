1eac0c1043c8891413e832040a80cda1b354f84c
Julieverrs <165794975+Julieverrs@users.noreply.github.com>
2025-11-16 15:22:14 +0800

    refactor: Use build.sh and Procfile for reliable deployment

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Run Python build steps
+python -m nltk.downloader -d /opt/nltk_data punkt stopwords
+
+# Run Node.js build steps
+npm install --ignore-scripts
+chmod +x ./node_modules/.bin/vite
+npm run build
