5298311d4cb2c7275d57b2af0ac08cbc5e3aa6c3
Julieverrs <165794975+Julieverrs@users.noreply.github.com>
2025-11-17 18:35:21 +0800

    10

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  root: './static',
-  base: '/static/',
+  // The root is the project root, where package.json is.
+  // No need to specify `root` if it's the default.
+  base: '/', // Serve from the root of the domain
   build: {
-    outDir: path.resolve(__dirname, 'employee/static/dist'),
+    // Build assets into a `dist` folder at the project root
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
     manifest: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'static/js/main.js'),
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'static/js')
-    }
   }
 })
