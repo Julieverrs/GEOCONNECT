cca9d96f41d025975b54f76e7177bb41b8027da8
Julieverrs <165794975+Julieverrs@users.noreply.github.com>
2025-11-17 18:50:37 +0800

    12

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [],
   // The root is the project root, where package.json is.
   // No need to specify `root` if it's the default.
   base: '/', // Serve from the root of the domain
